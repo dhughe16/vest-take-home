7a704bd4d55890543839708ccc429449eed7596e
Darcy Hughes <darcyhghs@gmail.com>
2025-08-25 16:36:13 -0500

    Add some visuals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,28 @@ export default function Home() {
         </Image>
       </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center justify-items-center p-4">
+        <div className="card bg-[#00808F] rounded-md p-3 w-[100%]">
+          <p >Total Portfolio Value</p>
+          <div className="text-2xl font-bold">$1,000,000</div>
+        </div>
         <div className="card bg-white rounded-sm p-3 w-[100%]">
           <MainChart/>
         </div>
+        <div className="summary-cards flex gap-[16px]">
+          <div className="card rounded-md p-3 bg-white text-gray-900">
+            <p className="text-sm text-greeen-600">Today's Δ</p>
+            <div className="text-xl font-bold">+$1,234.56</div>
+          </div>
+          <div className="card rounded-md p-3 bg-white text-gray-900">
+            <p className="text-sm">Top Performer</p>
+            <div className="text-xl font-bold">DASH</div>
+          </div>
+          <div className="card rounded-md p-3 bg-white text-gray-900">
+            <p className="text-sm">Total Portfolio Value</p>
+            <div className="text-xl font-bold">$1,234.56</div>
+          </div>
+        </div>
+        <button className="primary bg-[#FF7619] rounded-sm p-3 font-bold text-lg text-white">View Summary</button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center m-3">
         <p className="text-xs text-[#7db2c8d6]">
